36249caa13a565bf3c109eb516e7b4eafdbd0c32
vinhnh0715 <keito1507@gmail.com>
2024-04-12 17:40:28 +0700

    hotfix: forgot set method to static

--- lib/services/camera_services.dart
+++ lib/services/camera_services.dart
@@ -25,7 +25,7 @@ class CameraServices {
     }
   }
 
-  Future<void> sendAlert(List<int> imageData, List<int> videoData, String cameraId, int fireDetection) async {
+  static Future<void> sendAlert(List<int> imageData, List<int> videoData, String cameraId, int fireDetection) async {
     try {
       var now = DateTime.now();
       var formattedDateTime = '${now.day}-${now.month}-${now.year}-${now.hour}-${now.minute}-${now.second}';
@@ -34,11 +34,7 @@ class CameraServices {
         'POST',
         Uri.parse('$apiUrl/Camera/$cameraId/alert'),
       );
-
-      // Add authorization token to the headers
       request.headers['Authorization'] = 'Bearer $UserServices.accessToken';
-
-      // Add the image data as a file field to the request
       request.files.add(
         http.MultipartFile.fromBytes(
           'image',
@@ -46,8 +42,6 @@ class CameraServices {
           filename: 'incident_$formattedDateTime.jpg',
         ),
       );
-
-      // Add the video data as a file field to the request
       request.files.add(
         http.MultipartFile.fromBytes(
           'video',
@@ -55,11 +49,7 @@ class CameraServices {
           filename: 'incident_$formattedDateTime.mp4',
         ),
       );
-
-      // Add fire detection as a field
       request.fields['FireDetection'] = fireDetection.toString();
-
-      // Send the request
       var response = await http.Response.fromStream(await request.send());
 
       if (response.statusCode == 200) {
@@ -71,5 +61,4 @@ class CameraServices {
       print('Error in sendAlert: $e');
     }
   }
-
 }
